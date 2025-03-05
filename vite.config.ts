e3b4365c4c17fb7b926bac16f41a46db530ded14
rgerero <31179503+rgerero@users.noreply.github.com>
2025-03-05 20:49:21 +0800

    add ci/cd workflows

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // 👈 Set this to your GitHub repo name
   plugins: [react()],
 })
