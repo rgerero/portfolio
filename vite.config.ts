d2cd7ad242e3d2c39ad3ad9edaf1d5ec1fe20ce8
rgerero <31179503+rgerero@users.noreply.github.com>
2025-03-07 03:14:28 +0800

    update the vite.config.ts for netlify hosting.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // 👈 Set this to your GitHub repo name
+  //base: '/portfolio/', // 👈 Set this to your GitHub repo name
+  base: './', // 👈 Set this for netlify
   plugins: [react()],
 })
