e6fa14be84560efd442783c5899b469c560bd030
rgerero <31179503+rgerero@users.noreply.github.com>
2025-03-08 17:23:10 +0800

    update tsx file for deployment testing.

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Hi I'm Ryan</h2>
+      <h2>Hi I'm Alfred Ryan.</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
